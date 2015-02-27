7b31b0793491d9813ebb355f93dea393917fb8b7
cciissoooo <cciissoooo@gmail.com>
2015-02-27 15:14:58 -0500

    AddBinary Better

--- src/string/AddBinary.java
+++ src/string/AddBinary.java
@@ -2,60 +2,29 @@ package string;
 
 public class AddBinary {
 
-    public String addBinary(String a, String b) {
-        if(a==null || b==null) return null;
-        
-        int aLen=a.length();
-        int bLen=b.length();
-        int maxLen=aLen;
-        if(aLen > bLen){
-            b = addPadding(b,aLen-bLen);  
-            maxLen = aLen;
-        }else if(aLen < bLen){
-            a = addPadding(a,bLen-aLen);  
-            maxLen = bLen;
-        }
-        
-        boolean carry = false;
-        StringBuilder c = new StringBuilder();
-        for(int i=maxLen-1; i>=0; i--){
-            if(a.charAt(i)=='0' && b.charAt(i)=='0'){
-                if(carry){
-                	c.append('1');carry=false;
-                }else{
-                	c.append('0'); carry=false;
-                }
-            }else if(a.charAt(i)=='1' && b.charAt(i)=='1'){
-                if(carry){
-                	c.append('1'); carry=true;
-                }else{
-                	c.append('0');  carry=true;
-                }
-            }else if((a.charAt(i)=='0' && b.charAt(i)=='1') || (a.charAt(i)=='1' && b.charAt(i)=='0')){
-                if(carry){
-                	c.append('0'); carry=true;
-                }else{
-                	c.append('1');  carry=false;
-                }
-            }
-            else return null;
-        }
-        
-        if(carry) {
-        	c.append('1');        	
-        }
-        return c.reverse().toString();
-        
-    }
-    
-    public String addPadding(String s, int diff){
-        StringBuilder sb = new StringBuilder();
-        for(int i=1;i<=diff;i++){
-            sb.append("0");
-        } 
-        sb.append(s);
-        return sb.toString();
-    }
+	 public String addBinary(String a, String b) {
+	        char[] ac = a.toCharArray();
+	        char[] bc = b.toCharArray();
+	        
+	        int carry = 0;
+	        int sum = 0;
+	        int endA = ac.length-1;
+	        int endB = bc.length-1;
+	        StringBuilder sb = new StringBuilder();
+
+	        while (endA >= 0 || endB >= 0) {
+	            int i = endA >= 0 ? ac[endA] - '0' : 0;
+	            int j = endB >= 0 ? bc[endB] - '0' : 0;
+	            sb.append((carry + i + j) % 2);
+	            carry = (carry + i + j) / 2;
+	            endA--;
+	            endB--;
+	        }
+	        
+	        if(carry == 1) sb.append('1');
+	        
+	        return sb.reverse().toString();
+	    }
 	
 	
 	public static void main(String[] args) {
