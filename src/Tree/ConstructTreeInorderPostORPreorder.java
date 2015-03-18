efdc422b2360d5fe9877ba24d299a19b44abf338
cciissoooo <chuyu@Chuyus-MacBook-Pro.local>
2015-03-18 01:29:32 -0400

    construct tree
    construct tree

--- src/Tree/ConstructTreeInorderPostORPreorder.java
+++ src/Tree/ConstructTreeInorderPostORPreorder.java
@@ -1,6 +1,6 @@
 package Tree;
 
-public class ConstructTreeInorderPostorder {
+public class ConstructTreeInorderPostORPreorder {
 
 	/**
 	 * in-order: (left, root, right)   4 2 5  (1)  6 7 3 8
@@ -23,16 +23,16 @@ public class ConstructTreeInorderPostorder {
 	  6	  8
 */
 
-	public TreeNode buildTree(int[] inorder, int[] postorder) {
+	public TreeNode buildTreeInPost(int[] inorder, int[] postorder) {
         int inStart = 0;
         int inEnd = inorder.length-1;
         int postStart =0;
         int postEnd = postorder.length-1;
  
-        return buildTree(inorder, inStart, inEnd, postorder, postStart, postEnd);
+        return buildTreeInPost(inorder, inStart, inEnd, postorder, postStart, postEnd);
     }
 	
-	 public TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd){
+	 public TreeNode buildTreeInPost(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd){
 		if(postStart > postEnd)
 			return null;
 		
@@ -45,10 +45,10 @@ public class ConstructTreeInorderPostorder {
 		int leftsize = k-inStart;
 		
 		//set inorder left sub-tree, and postorder left sub-tree
-		root.left = buildTree(inorder, inStart, k-1, postorder, postStart, postStart+leftsize-1);
+		root.left = buildTreeInPost(inorder, inStart, k-1, postorder, postStart, postStart+leftsize-1);
 				
 		//set inorder right sub-tree, and postorder right sub-tree
-		root.right = buildTree(inorder, k+1, inEnd, postorder, postStart+leftsize, postEnd-1);
+		root.right = buildTreeInPost(inorder, k+1, inEnd, postorder, postStart+leftsize, postEnd-1);
 		
 		return root;
 		}
@@ -68,6 +68,44 @@ public class ConstructTreeInorderPostorder {
 	    TreeNode right;
 	    TreeNode(int x) { val = x; }
 	}
+
+	/**
+	 * first element in pre-order arr is root
+	 * 
+	 * pre-order: (root, left, right) (1) 2 4 5 , 3 7 6 8  
+	 * 								   	  \   /   \     /
+	 *           			  	left sub-tree ,	right sub-tree	
+	 */
+	public TreeNode buildTreeInPre(int[] preorder, int[] inorder) {
+        return buildTreeInPre(inorder, 0, inorder.length-1, preorder, 0, preorder.length-1);
+    }
+    
+	
+	 public TreeNode buildTreeInPre(int[] inorder, int inStart, int inEnd, int[] preorder, int preStart, int preEnd){
+		if(preStart > preEnd)
+			return null;
+		
+		//first element in pre-order arr is root
+		int rootValue = preorder[preStart];
+		TreeNode root = new TreeNode(rootValue);
+		
+		//find the root in in-order array
+		int k=findIndex(inorder,rootValue, inStart, inEnd);		
+		int leftsize = k-inStart;
+		
+		//no difference for inorder's start and end
+		//set inorder left sub-tree, and preorder left sub-tree
+		root.left = buildTreeInPre(inorder, inStart, k-1, preorder, preStart+1, preStart+leftsize);
+				
+		//set inorder right sub-tree, and preorder right sub-tree
+		root.right = buildTreeInPre(inorder, k+1, inEnd, preorder, preStart+leftsize+1, preEnd);
+		
+		return root;
+		}
+	 
+	 
+	
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
