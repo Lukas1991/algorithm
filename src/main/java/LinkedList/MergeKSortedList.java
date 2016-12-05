898716f71e03b013fe8f39010afa66dffdca211f
Chuyu <chuyu@Chuyus-MacBook-Pro.local>
2016-12-04 20:58:41 -0500

    Merge K sorted list

--- src/main/java/LinkedList/MergeKSortedList.java
+++ src/main/java/LinkedList/MergeKSortedList.java
@@ -1,46 +1,31 @@
 package LinkedList;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class MergeKSortedList {
 	/**
 	 * Merge k sorted linked lists and return it as one sorted list. Analyze and
 	 * describe its complexity.
-	 * 
-	 * @param args
+	 *
 	 */
 
-	public ListNode mergeKLists(ArrayList<ListNode> lists) {
-		if (lists == null || lists.size() == 0) {
+	public ListNode mergeKLists(ListNode[] lists) {
+		if (lists == null || lists.length == 0) {
 			return null;
 		}
 
 		ListNode head = new ListNode(0);
 		ListNode current = head;
 		// accending order:
-		PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.size(),
-				new Comparator<ListNode>() {
+		PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.length, (o1, o2) -> o1.val - o2.val);
 
-					@Override
-					public int compare(ListNode o1, ListNode o2) {
-						if (o1.val > o2.val)
-							return 1;
-						else if (o1.val < o2.val)
-							return -1;
-						else
-							return 0;
-					}
-
-				});
 		// QUEUE doesn't allow null
 		for (ListNode list : lists) {
 			if (list != null)
 				pq.offer(list);
 		}
 
-		while (pq.size() > 0) {
+		while (!pq.isEmpty()) {
 			// get smallest ListNode in the queue
 			ListNode temp = pq.poll();
 			current.next = temp;
@@ -50,13 +35,7 @@ public class MergeKSortedList {
 			}
 		}
 
-		current.next = null;
 		return head.next;
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
