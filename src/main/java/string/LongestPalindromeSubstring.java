7cd9e2e454b3a1b879a2f34ab99162996fb835a0
Chuyu <chuyu@ang-mbp1.ip-soft.net>
2017-01-03 15:56:36 -0500

    longest Palindrome substring

--- src/main/java/string/LongestPalindromeSubstring.java
+++ src/main/java/string/LongestPalindromeSubstring.java
@@ -7,49 +7,38 @@ public class LongestPalindromeSubstring {
 	 * get longest palindrome with center of i, i+1
 	 * time O(n^2), space O(1)	
 	 */
-	public String helper(String s, int begin, int end) {
-		while (begin >= 0 && end <= s.length() - 1 && s.charAt(begin) == s.charAt(end)) {
-			begin--;
-			end++;
-		}
-		return s.substring(begin + 1, end);
-	}
-	    
-	    public String longestPalindrome(String s) {
-	        if (s.isEmpty()) {
+	public String longestPalindrome(String s) {
+		if (s == null) {
 			return null;
 		}
-	 
-		if (s.length() == 1) {
+		if (s.length() <= 1) {
 			return s;
 		}
-	 
-		String longest = s.substring(0, 1);
-		for (int i = 0; i < s.length(); i++) {
+
+		String longest = "";
+		for (int i = 0; i < s.length() - 1; i++) {
 			// get longest palindrome with center of i
-			String tmp = helper(s, i, i);
+			String tmp = getPalindrome(s, i, i);
 			if (tmp.length() > longest.length()) {
 				longest = tmp;
 			}
 	 
 			// get longest palindrome with center of i, i+1
-			tmp = helper(s, i, i + 1);
+			tmp = getPalindrome(s, i, i + 1);
 			if (tmp.length() > longest.length()) {
 				longest = tmp;
 			}
 		}
 	 
 		return longest;
-	    }
-	
-/**
- * time O(n^2), space O(1)	
- * @param args
- */
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	}
 
+	private String getPalindrome(String s, int begin, int end) {
+		while (begin >= 0 && end < s.length() && s.charAt(begin) == s.charAt(end)) {
+			begin--;
+			end++;
+		}
+		return s.substring(begin + 1, end);
 	}
 
 }
