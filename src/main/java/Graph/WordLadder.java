ce4066a51e9c1efba0cde74019f6bc006794cdd5
Chuyu <chuyu@Chuyus-MacBook-Pro.local>
2017-01-12 11:52:18 -0500

    Word Ladder

--- src/main/java/Graph/WordLadder.java
+++ src/main/java/Graph/WordLadder.java
@@ -1,7 +1,10 @@
 package Graph;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 public class WordLadder {
 
@@ -9,61 +12,134 @@ public class WordLadder {
 	 * Use two queues, one for words and another for counting
 	 * 
 	 */
-	
-	public static int ladderLength(String start, String end, HashSet<String> dict) {
-		if (dict.size() == 0)
-			return 0;
-	 
-		dict.add(end);
-	 
-		LinkedList<String> wordQueue = new LinkedList<String>();
-		LinkedList<Integer> distanceQueue = new LinkedList<Integer>();
-	 
-		wordQueue.add(start);
+	public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+		if (beginWord.equals(endWord)) return 1;
+
+		LinkedList<String> wordQueue = new LinkedList<>();
+		LinkedList<Integer> distanceQueue = new LinkedList<>();
+
+		wordQueue.add(beginWord);
 		distanceQueue.add(1);
-	 
-		//track the shortest path
-		int result = Integer.MAX_VALUE;
+		wordList.remove(beginWord);
+
+		int minStep = Integer.MAX_VALUE;
 		while (!wordQueue.isEmpty()) {
-			String currWord = wordQueue.pop();
-			System.out.println("----de Q: "+currWord);
-			
-			Integer currDistance = distanceQueue.pop();
-	 
-			if (currWord.equals(end)) {
-				result = Math.min(result, currDistance);
+			String word = wordQueue.pop();
+			int currDistance = distanceQueue.pop();
+
+			if (word.equals(endWord)) {
+				minStep = Math.min(minStep, currDistance);
+				continue;
 			}
-	 
-			for (int i = 0; i < currWord.length(); i++) {
-				char[] currCharArr = currWord.toCharArray();
-				for (char c = 'a'; c <= 'z'; c++) {
-					currCharArr[i] = c;
-	 
-					String newWord = new String(currCharArr);
-					if (dict.contains(newWord)) {
-						wordQueue.add(newWord);
-						System.out.println("In Q: "+newWord);
-						distanceQueue.add(currDistance + 1);
-						dict.remove(newWord);
+
+			for (int i=0; i<word.length(); i++) {
+				char[] begins = word.toCharArray();
+
+				for (char c = 'a'; c<='z'; c++) {
+					if (c != begins[i]) {
+						char tmp = begins[i];
+						begins[i] = c;
+						String newStr = new String(begins);
+						if (wordList.contains(newStr)) {
+							wordList.remove(newStr);
+
+							wordQueue.add(newStr);
+							distanceQueue.add(currDistance + 1);
+						}
+						begins[i] = tmp;
 					}
 				}
 			}
 		}
-	 
-		if (result < Integer.MAX_VALUE)
-			return result;
-		else
-			return 0;
+
+		if (minStep == Integer.MAX_VALUE) return 0;
+		return minStep;
 	}
-	
-	
+
+	class WordNode {
+		String word;
+		int steps;
+		WordNode pre;
+
+		public WordNode(String word, int steps, WordNode pre) {
+			this.word = word;
+			this.steps = steps;
+			this.pre = pre;
+		}
+	}
+
+	public List<List<String>> findLadders(String beginWord, String endWord, Set<String> wordList) {
+		List<List<String>> res = new ArrayList<>();
+		LinkedList<WordNode> queue = new LinkedList<>();
+		queue.add(new WordNode(beginWord, 1, null));
+
+		wordList.add(endWord);
+		int minStep = 0;
+
+		Set<String> visited = new HashSet<>();
+		Set<String> unVisited = new HashSet<>();
+		unVisited.addAll(wordList);
+
+		int preNumSteps = 0;
+
+		while (!queue.isEmpty()) {
+			WordNode node = queue.pop();
+			int currentSteps = node.steps;
+			String word = node.word;
+
+			if (word.equals(endWord)) {
+				if (minStep == 0) {
+					minStep = currentSteps; //is 1
+				}
+
+				if (currentSteps == minStep) {
+					List<String> list = new ArrayList<>();
+					list.add(word);
+					while (node.pre != null) {
+						node = node.pre;
+						list.add(0, node.word); //add to first
+					}
+					res.add(list);
+					continue;
+				}
+			}
+
+			if (preNumSteps < currentSteps) {
+				unVisited.removeAll(visited);
+			}
+			preNumSteps = currentSteps;
+
+			char[] arr = word.toCharArray();
+			for (int i=0; i<word.length(); i++) {
+				for (char c = 'a'; c <= 'z'; c++) {
+					char tmp = arr[i];
+					if (arr[i] != c) {
+						arr[i] = c;
+					}
+					String newWord = new String(arr);
+					if (unVisited.contains(newWord)) {
+						queue.add(new WordNode(newWord, currentSteps + 1, node));
+						visited.add(word);
+					}
+					arr[i] = tmp;
+				}
+
+			}
+
+		}
+
+		return res;
+	}
+
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WordLadder wordLadder = new WordLadder();
+
 		HashSet<String> dict = new HashSet<String>();
+		System.err.println(dict.toString());
 		dict.add("hot");dict.add("dot");dict.add("dog");dict.add("lot");dict.add("log");
-		ladderLength("hit", "cog", dict);
-		
-		
+		wordLadder.ladderLength("hit", "cog", dict);
+
 	}
 
 }
