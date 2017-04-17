1083ec9f7d961bd8a7650dc0e175312daddfed6b
Chuyu <chuyu@Chuyus-MacBook-Pro.local>
2017-04-17 15:02:58 -0400

    Valid Parentheses

---
+++ src/main/java/StackAndQueue/ValidParentheses.java
@@ -0,0 +1,35 @@
+package StackAndQueue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            //is open bracket
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                //is closed bracket
+                if (stack.isEmpty() || map.get(stack.pop()) != c) {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
