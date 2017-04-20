e1a85d9ca6cc7d47894a217d9acdd2b66a72f6f1
chuyu <chuyu@192.168.0.103>
2017-04-19 21:11:47 -0400

    permute

--- src/main/java/array/Permutation.java
+++ src/main/java/array/Permutation.java
@@ -1,6 +1,7 @@
 package array;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Permutation {
 
@@ -41,16 +42,50 @@ public class Permutation {
         return prod;
     }
 	
+	public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 1) {
+            List<Integer> newlist = new ArrayList<>();
+            newlist.add(nums[0]);
+            res.add(newlist);
+            return res;
+        }
+        
+        for (int i=0; i<nums.length; i++) {
+            //starts with i
+            int[] arr = new int[nums.length - 1];
+            for (int j=0; j<i; j++) {
+                arr[j] = nums[j];
+            }
+            for (int j=i+1; j<nums.length; j++) {
+                arr[j-1] = nums[j];
+            }
+            
+            List<List<Integer>> others = permute(arr);
+            for(List<Integer> list : others) {
+                List<Integer> newlist = new ArrayList<>();
+                newlist.add(nums[i]);
+                newlist.addAll(list);
+                res.add(newlist);
+            }
+            
+        }
+        
+        return res;
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Permutation obj = new Permutation();
+		int[] arr = {2,3};
+		System.out.println(obj.permute(arr));
 		//System.out.println(obj.factorial(0));
 		//System.out.println(obj.factorial(1));
 		//System.out.println(obj.factorial(2));
 		//System.out.println(obj.factorial(3));
 		//System.out.println(obj.factorial(4));
-		System.out.println(0%2);
-		System.out.println(obj.getPermutation(3,4));
+		//System.out.println(0%2);
+		//System.out.println(obj.getPermutation(3,4));
 	}
 
 }
