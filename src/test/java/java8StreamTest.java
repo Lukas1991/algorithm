89f0b3c621580c66db92ca1c60d8a8c7015f338a
Chuyu <chuyu@Chuyus-MacBook-Pro-4.local>
2017-08-08 12:10:48 -0400

    groupingBy, distinct

--- src/test/java/java8StreamTest.java
+++ src/test/java/java8StreamTest.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 public class java8StreamTest {
@@ -20,5 +22,28 @@ public class java8StreamTest {
 
         int sum = list.stream().mapToInt(Integer::intValue).sum();
         System.err.println(sum);
+    }
+
+    @Test
+    public void groupingBy() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        Map<Integer, List<Integer>> map = list.stream().collect(Collectors.groupingBy(i -> i % 2));
+
+        System.err.println(map.toString());
+    }
+
+    @Test
+    public void distinct() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(1);
+
+        List<Integer> distinctList = list.stream().distinct().collect(Collectors.toList());
+        System.err.println(distinctList.toString());
     }
 }
