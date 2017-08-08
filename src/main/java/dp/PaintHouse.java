66bc38c582a2cecceff1ae896deb01b1d1416c2d
Chuyu <chuyu@Chuyus-MacBook-Pro-4.local>
2017-08-08 16:30:54 -0400

    O(1) space

--- src/main/java/dp/PaintHouse.java
+++ src/main/java/dp/PaintHouse.java
@@ -41,11 +41,13 @@ public class PaintHouse {
      * cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the
      * minimum cost to paint all houses.
      *
-     * Could you solve it in O(nk) runtime?
+     * Could you solve it in O(nk) runtime, in O(1) space?
      *
      * same as how to get previous house's min in O(1)
      * We can use min1 and min2 to track the indices of the smallest and 2nd smallest cost of the previous house,
      * if the current house's color index is same as min1, then we have to go with min2, otherwise we can safely go with min1.
+     *
+     * use costs array as the dp array will reduce the space to O(1)
      */
     public static int minCostII(int[][] costs) {
         int n = costs.length;
@@ -53,8 +55,6 @@ public class PaintHouse {
             return 0;
         }
         int k = costs[0].length;
-
-        int[][] dp = new int[n][k];
 
         int currMin1Index = -1;//index of the 1st-smallest cost
         int currMin2Index = -1;//index of the 2nd-smallest cost
@@ -68,27 +68,27 @@ public class PaintHouse {
 
             for (int j = 0; j < k; j++) {
                 if (i == 0) {
-                    dp[0][j] = costs[0][j];
+                    costs[0][j] = costs[0][j];
                 } else {
 
                     if (j == preMin1Index) {
-                        dp[i][j] = dp[i - 1][preMin2Index] + costs[i][j];
+                        costs[i][j] = costs[i - 1][preMin2Index] + costs[i][j];
                     } else {
-                        dp[i][j] = dp[i - 1][preMin1Index] + costs[i][j];
+                        costs[i][j] = costs[i - 1][preMin1Index] + costs[i][j];
                     }
                 }
 
                 //re-calculate min1, min2
-                if (currMin1Index < 0 || dp[i][j] <= dp[i][currMin1Index]) {
+                if (currMin1Index < 0 || costs[i][j] <= costs[i][currMin1Index]) {
                     currMin2Index = currMin1Index;
                     currMin1Index = j;
-                } else if (currMin2Index < 0 || dp[i][j] < dp[i][currMin2Index]) {
+                } else if (currMin2Index < 0 || costs[i][j] < costs[i][currMin2Index]) {
                     currMin2Index = j;
                 }
             }
         }
 
-        return dp[n - 1][currMin1Index];
+        return costs[n - 1][currMin1Index];
     }
 
     public static void main(String[] args) {
