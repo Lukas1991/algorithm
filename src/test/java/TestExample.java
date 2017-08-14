f5c42a91e4caaf3de141a240e77cd12384457b45
Chuyu <chuyu@Chuyus-MacBook-Pro-4.local>
2017-08-14 17:35:03 -0400

    test

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,13 +1,46 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 
 public class TestExample {
 
     @Test
     public void test() {
-        int[] arr = {1, 2, 3, 4};
+        List<String> res = addOperators("123", 6);
+        System.err.println(res.toString());
+//        res = addOperators("105", 5);
+//        System.err.println(res.toString());
 
-        System.err.println(Arrays.toString(arr));
+    }
+
+    public List<String> addOperators(String num, int target) {
+        List<String> rst = new ArrayList<String>();
+        if(num == null || num.length() == 0) return rst;
+        helper(rst, "", num, target, 0, 0, 0);
+        return rst;
+    }
+    public void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
+        System.err.println(path + " pos: " + pos + " eval: " + eval + " multed: " + multed);
+        if(pos == num.length()){
+            if(target == eval)
+                rst.add(path);
+            return;
+        }
+        for(int i = pos; i < num.length(); i++){
+            if(i != pos && num.charAt(pos) == '0') break;
+            long cur = Long.parseLong(num.substring(pos, i + 1));
+
+            if(pos == 0){
+                helper(rst, path + cur, num, target, i + 1, cur, cur);
+            }
+            else{
+                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
+
+                helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
+
+                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+            }
+        }
     }
 
 }
