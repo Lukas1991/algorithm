4e5514fc8d72f82063d9861729da6f5eee30c4d8
Chuyu <chuyu@Chuyus-MBP-4.fios-router.home>
2017-09-04 12:21:24 -0400

    MultiThread Test

---
+++ src/main/java/tools/MultiThreadTest.java
@@ -0,0 +1,44 @@
+package tools;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MultiThreadTest {
+
+    private static AtomicInteger counter = new AtomicInteger(0);
+
+    static class MyRunnable implements Runnable {
+
+        public void run() {
+            List<Integer> list = get();
+            System.err.println("Thread " + Thread.currentThread().getId() + list.toString());
+        }
+
+        //try without synchronized
+        private synchronized List<Integer> get() {
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 0; i<5; i++) {
+                list.add(counter.getAndIncrement());
+            }
+
+            return list;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(new MyRunnable());
+        Thread t2 = new Thread(new MyRunnable());
+        t1.start();
+        t2.start();
+
+        Thread t3 = new Thread(new MyRunnable());
+        Thread t4 = new Thread(new MyRunnable());
+        t3.start();
+        t4.start();
+    }
+
+}
