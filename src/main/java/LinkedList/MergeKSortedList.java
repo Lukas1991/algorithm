b5d11fa35dae4a7c65bc9a79d785bcb870edf8a9
Chuyu <chuyu@Chuyus-MBP-4.fios-router.home>
2017-09-05 12:09:42 -0400

    MergeKSortedList

--- src/main/java/LinkedList/MergeKSortedList.java
+++ src/main/java/LinkedList/MergeKSortedList.java
@@ -3,39 +3,39 @@ package LinkedList;
 import java.util.PriorityQueue;
 
 public class MergeKSortedList {
-	/**
-	 * Merge k sorted linked lists and return it as one sorted list. Analyze and
-	 * describe its complexity.
-	 *
-	 */
 
-	public ListNode mergeKLists(ListNode[] lists) {
-		if (lists == null || lists.length == 0) {
-			return null;
-		}
+    /**
+     * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
 
-		ListNode head = new ListNode(0);
-		ListNode current = head;
-		// accending order:
-		PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.length, (o1, o2) -> o1.val - o2.val);
+        // accending order:
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, (o1, o2) -> o1.val - o2.val);
 
-		// QUEUE doesn't allow null
-		for (ListNode list : lists) {
-			if (list != null)
-				pq.offer(list);
-		}
+        // QUEUE doesn't allow null
+        for (ListNode list : lists) {
+            if (list != null) {
+                pq.offer(list);
+            }
+        }
 
-		while (!pq.isEmpty()) {
-			// get smallest ListNode in the queue
-			ListNode temp = pq.poll();
-			current.next = temp;
-			current = current.next;
-			if (temp.next != null) {
-				pq.offer(temp.next);
-			}
-		}
+        ListNode head = new ListNode(0);
+        ListNode current = head;
 
-		return head.next;
-	}
+        while (!pq.isEmpty()) {
+            // get smallest ListNode in the queue
+            ListNode temp = pq.poll();
+            current.next = temp;
+            current = current.next;
+            if (temp.next != null) {
+                pq.offer(temp.next);
+            }
+        }
+
+        return head.next;
+    }
 
 }
