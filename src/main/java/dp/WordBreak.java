cf09592942c3208cd1dad6510500403e66ea29f3
Chuyu <chuyu@Chuyus-MBP-4.fios-router.home>
2017-09-05 19:12:10 -0400

    WordBreak

--- src/main/java/dp/WordBreak.java
+++ src/main/java/dp/WordBreak.java
@@ -1,142 +1,132 @@
 package dp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class WordBreak {
 
-	/**
-	 * Define an array main.java.dp[](size is s.length+1) such that main.java.dp[i]==true indicates 0-(i-1) can be segmented using dictionary
-	 * Initial state main.java.dp[0] == true, return main.java.dp[s.length()]
-	 * main.java.dp[k] refers to s.substring(0,k)
-	 * O(n^2)
-	 * @param args
-	 */
-	public boolean wordBreak(String s, Set<String> dict) {
-		
-		boolean dp[] = new boolean[s.length()+1];
-		dp[0] = true; //set initial state to be true
-		
-		for(int i=1; i<=s.length(); i++){
-			for(int k=0; k<i; k++){
-				//if main.java.dp[k] refers to s.substring(0,k)
-				if(dp[k] && dict.contains(s.substring(k, i))){
-					dp[i] = true;
-					break;
-				}
-				
-			}
-		}
-		
-		return dp[s.length()];
-		
-	}
-	/**
-	 * use a main.java.dp array, List<String>[], to track actual words. when main.java.dp[i] not null, indicates 0-(i-1) can be segmented,
-	 * main.java.dp[i] contains the substrings in dict, which start from any previous segmented index, end to i-1
-	 * which means: main.java.dp[k]!=null && dict.contains(s.substring(k, i))
-	 * 
-	 * After main.java.dp array is ready, dfs, root is the end word, "dog", find all paths from end to start
-	 * Test main.java.dp[]:
-	 * index: 0  
-	 * index: 3  cat, 
-	 * index: 4  cats, 
-	 * index: 7  sand, and, 
-	 * index: 10  dog,
-	 */
+    /**
+     * Use a cache. map.get(k) indicates start at k.
+     * Time complexity : O(n^2).
+     * Space complexity : O(n).
+     */
+    public boolean wordBreak1Best(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        booleanMap.put(s.length(), true);
+        return helper(s, 0, dict);
+    }
 
-	public static List<String> wordBreak2(String s, Set<String> dict) {
-	    //create an array of ArrayList<String>
-	    List<String>[] dp = new ArrayList[s.length()+1];
-	    dp[0] = new ArrayList<String>(); //set initial state to be not null
-	 
-	    //if main.java.dp[k]==null, s.substring(0,k) cannot be segmented
-	    for(int i=1; i<=s.length(); i++){
-	    	for(int k=0; k<i; k++){
-	    		//if s.substring(0,k) can be segmented, and s.substring(k, i) in dict, add s.substring(k, i) into list main.java.dp[i]
-	    		if(dp[k]!=null && dict.contains(s.substring(k, i))){
-	    			if(dp[i]==null){
-	    				dp[i] = new ArrayList<String>();
-	    			}
-	    			dp[i].add(s.substring(k, i));
-	    		}
-	    	}
-	    }
-	    
-	/*    
-	    System.out.println("Test main.java.dp[]: ");
-	    for(int i=0; i<main.java.dp.length; i++){
-	    	List<String> list = main.java.dp[i];
-	    	if(list != null){
-	    		System.out.print("index: "+i+"  ");
-	    		for(String str:list){
-	    			System.out.print(str+", ");
-	    		}
-	    		System.out.print("\n");
-	    	}
-	    }
-	  */  
-	    
-	    
-	    List<String> result = new LinkedList<String>();
-	    if(dp[s.length()] == null)  //cannot be segmented
-	        return result; 
-	 
-	    ArrayList<String> temp = new ArrayList<String>();
-	    //dfs, root is the end word, "dog", find all paths from end to start
-	    dfs(dp, s.length(), result, temp);
-	 
-	    return result;
-	}
-	 
-	public static void dfs(List<String> dp[],int end,List<String> result, ArrayList<String> tmp){
-		//return condition
-		if(end<=0){
-			//tmp will have dog, and, cats
-			String res="";
-			for(int i=tmp.size()-1; i>=0; i--){
-				res = res+tmp.get(i);
-				if(i>0) res = res+" ";
-			}
-			result.add(res);
-		}
-				
-		
-		for(String str : dp[end]){
-			tmp.add(str);
-			dfs(dp, end-str.length(), result, tmp);
-			//remove by index, the last one just added
-			tmp.remove(tmp.size()-1);
-		}
-		
-		
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Set<String> dict = new HashSet<String>();
-		dict.add("cat");
-		dict.add("cats");
-		dict.add("and");
-		dict.add("sand");
-		dict.add("dog");
-		
-		wordBreak2("catsanddog", dict);
-		Set<String> dict1 = new HashSet<String>();
-		dict1.add("aaaa");
-		dict1.add("aa");
-		//wordBreak2("aaaaaa", dict1);
-		
-		ArrayList<String> tmp = new ArrayList<String>();
-		tmp.add("dog");tmp.add("cats");tmp.add("and");tmp.add("cats");
-		tmp.remove("cats");
-		System.out.println(tmp.size());
-		for(String s: tmp){
-			System.out.println(s);
-		}
-	}
+    //key - start index, value - s.substring(start) can break?
+    Map<Integer, Boolean> booleanMap = new HashMap<>();
+
+    private boolean helper(String s, int start, Set<String> dict) {
+        if (booleanMap.containsKey(start)) {
+            return booleanMap.get(start);
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            String sub = s.substring(start, i + 1);
+            if (dict.contains(sub)) {
+                if (helper(s, i + 1, dict)) {
+                    booleanMap.put(start, true);
+                    return true;
+                }
+            }
+        }
+        booleanMap.put(start, false);
+        return false;
+    }
+
+    /**
+     * Use dp[], dp[k]==true indicates ends at k, s.substring(0,k) can be segmented using dictionary.
+     * Initial state dp[0] == true, return dp[s.length()].
+     * Time complexity : O(n^2).
+     * Space complexity : O(n).
+     */
+    public boolean wordBreak1DP(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        int n = s.length();
+
+        boolean dp[] = new boolean[n + 1];
+        dp[0] = true; //set initial state to be true
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 0; k < i; k++) {
+                if (dp[k] && dict.contains(s.substring(k, i))) {
+                    dp[i] = true;
+                    break;
+                }
+
+            }
+        }
+
+        return dp[n];
+    }
+
+
+    /**
+     * Use a cache.
+     * Time complexity : O(n^3).
+     * Space complexity : O(n^3).
+     */
+    public List<String> wordBreak2(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        List<String> empty = new ArrayList<>();
+        empty.add("");
+        map.put(s.length(), empty);
+        return word_Break(s, dict, 0);
+    }
+
+    //key - start index, value -  word break of s.substring(start)
+    HashMap<Integer, List<String>> map = new HashMap<>();
+
+    public List<String> word_Break(String s, Set<String> wordDict, int start) {
+        if (map.containsKey(start)) {
+            return map.get(start);
+        }
+        List<String> res = new ArrayList<>();
+
+        for (int end = start + 1; end <= s.length(); end++) {
+            String sub = s.substring(start, end);
+            if (wordDict.contains(sub)) {
+                List<String> list = word_Break(s, wordDict, end);
+
+                for (String l : list) {
+                    if (l.equals("")) {
+                        res.add(sub);
+                    } else {
+                        res.add(sub + " " + l);
+                    }
+                }
+            }
+        }
+
+        map.put(start, res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        WordBreak obj = new WordBreak();
+
+        List<String> dict = new ArrayList<>();
+        dict.add("cat");
+        dict.add("cats");
+        dict.add("and");
+        dict.add("sand");
+        dict.add("dog");
+
+        String str = "catsanddog";
+
+        System.err.println(obj.wordBreak1Best(str, dict));
+        List<String> res = obj.wordBreak2(str, dict);
+
+        for (String s : res) {
+            System.err.println(s);
+        }
+    }
 
 }
