9064b21a24cdca31bbad0e6accb2b8a9c071262a
Chuyu <chuyu@Chuyus-MacBook-Pro-4.local>
2017-09-06 15:52:26 -0400

    BasicCalculator

--- src/main/java/StackAndQueue/BasicCalculator.java
+++ src/main/java/StackAndQueue/BasicCalculator.java
@@ -1,11 +1,14 @@
 package StackAndQueue;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class BasicCalculator {
 
     /**
+     * 224. Basic Calculator.
      * Implement a basic calculator to evaluate a simple expression string.
+     * The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
      * " 2-1 + 2 " = 3
      * "(1+(4+5+2)-3)+(6+8)" = 23
      */
@@ -55,7 +58,7 @@ public class BasicCalculator {
     }
 
     //2 stacks
-    public int calculate2(String s) {
+    public int calculate2Stacks(String s) {
         Stack<Character> opStack = new Stack<>();
         Stack<Integer> valueStack = new Stack<>();
         int i = 0;
@@ -118,9 +121,78 @@ public class BasicCalculator {
         return tmp;
     }
 
+    /**
+     * 227. Basic Calculator II.
+     * The expression string contains only non-negative integers, +, -, *, / operators and empty spaces .
+     * "3+2*2" = 7
+     * " 3/2 " = 1
+     * " 3+5 / 2 " = 5
+     * LinkedList is double ended queue
+     */
+    public int calculate2(String s) {
+        LinkedList<Character> ops = new LinkedList<>();
+        LinkedList<Integer> values = new LinkedList<>();
+
+        int i = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                String number = "" + c;
+                i++;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    number += s.charAt(i);
+                    i++;
+                }
+
+                values.addLast(Integer.valueOf(number));
+            } else if (c == '+' || c == '-') {
+                ops.addLast(c);
+                i++;
+            } else if (c == '*' || c == '/') {
+                int previous = values.removeLast();
+                String number = "";
+
+                i++;
+                while (i < s.length() && (s.charAt(i) == ' ' || Character.isDigit(s.charAt(i)))) {
+                    if (Character.isDigit(s.charAt(i))) {
+                        number += s.charAt(i);
+                    }
+                    i++;
+                }
+
+                int next = Integer.valueOf(number);
+
+                if (c == '*') {
+                    values.addLast(previous * next);
+                } else {
+                    values.addLast(previous / next);
+                }
+            } else {
+                i++;
+            }
+        }
+
+
+        int res = values.removeFirst();
+
+        while (!ops.isEmpty()) {
+            char c = ops.removeFirst();
+            int value = values.removeFirst();
+            if (c == '+') {
+                res += value;
+            } else {
+                res -= value;
+            }
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         BasicCalculator obj = new BasicCalculator();
         System.err.println(obj.calculate("(1+(4+5+2)-3)+(6+8)"));  //23
         System.err.println(obj.calculate(" 2-1 + 2 ")); //3
+
+        System.err.println(obj.calculate2("3+2*2 - 4 / 2")); // 5
     }
 }
