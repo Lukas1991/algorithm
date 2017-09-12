61679128a919672317d6b9814280b6afe6e6e41c
Chuyu <chuyu@Chuyus-MacBook-Pro-4.local>
2017-09-12 10:23:59 -0400

    min stack

--- src/main/java/StackAndQueue/MinStack.java
+++ src/main/java/StackAndQueue/MinStack.java
@@ -11,6 +11,30 @@ public class MinStack {
         minStack = new Stack<>();
     }
 
+    //Note: when minStack is empty, x <= minStack.peek
+    public void push(int x) {
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+        stack.push(x);
+    }
+
+    //Note, compare two Integer
+    public void pop() {
+        int pop = stack.pop();
+        if (minStack.peek() == pop) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
     public static void main(String[] args) {
         System.err.println(2 < new Integer(3)); //true
         System.err.println(3 == new Integer(3)); //true
@@ -28,29 +52,5 @@ public class MinStack {
         System.err.println(obj.getMin());
         obj.pop();
         System.err.println(obj.getMin());
-    }
-
-    //Note: when minStack is empty, x <= minStack.peek
-    public void push(int x) {
-        if (minStack.isEmpty() || x <= minStack.peek()) {
-            minStack.push(x);
-        }
-        stack.push(x);
-    }
-
-    //Note, compare two Integer
-    public void pop() {
-        if (stack.peek().intValue() == minStack.peek().intValue()) {
-            minStack.pop();
-        }
-        stack.pop();
-    }
-
-    public int top() {
-        return stack.peek();
-    }
-
-    public int getMin() {
-        return minStack.peek();
     }
 }
