efbdaa5f60bb5d0305bde57e1c2156974bf6fee7
Chuyu <chuyu@Chuyus-MacBook-Pro-4.local>
2017-09-20 17:19:45 -0400

    UniqueBinarySearchTrees

---
+++ src/main/java/dp/UniqueBinarySearchTrees.java
@@ -0,0 +1,70 @@
+package dp;
+
+import Tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueBinarySearchTrees {
+
+    /**
+     * 96. Unique Binary Search Trees
+     */
+    public int numTrees(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                //j is root
+                int left = dp[j - 1];
+                int right = dp[i - j];
+                count += left * right;
+            }
+            dp[i] = count;
+        }
+
+        return dp[n];
+    }
+
+    /**
+     * 95. Unique Binary Search Trees II.
+     * Recursion.
+     */
+    public List<TreeNode> generateTrees(int n) {
+        if (n <= 0) {
+            return new ArrayList<>();
+        } else {
+            return generateTrees(1, n);
+        }
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> res = new ArrayList<>();
+
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> lefts = generateTrees(start, i - 1);
+            List<TreeNode> rights = generateTrees(i + 1, end);
+
+            //count = lefts * rights
+            for (TreeNode left : lefts) {
+                for (TreeNode right : rights) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(root);
+                }
+            }
+        }
+
+        return res;
+    }
+}
