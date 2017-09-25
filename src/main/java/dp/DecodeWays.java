345b80bb858d47567491efe4303fc6b86edc2c81
Chuyu <chuyu@Chuyus-MBP-4.fios-router.home>
2017-09-24 21:00:02 -0400

    Decode ways

--- src/main/java/dp/DecodeWays.java
+++ src/main/java/dp/DecodeWays.java
@@ -17,13 +17,12 @@ public class DecodeWays {
         }
 
         int[] dp = new int[n + 1];
-
         dp[n] = 1;
         dp[n - 1] = s.charAt(n - 1) == '0' ? 0 : 1;
 
         for (int i = n - 2; i >= 0; i--) {
             if (s.charAt(i) == '0') {
-                dp[i] = dp[i + 1];  //or dp[i] == 0;
+                dp[i] = 0; //or dp[i] = dp[i+1] if leading zeros are valid
             } else if (Integer.valueOf(s.substring(i, i + 2)) <= 26) {
                 dp[i] = dp[i + 1] + dp[i + 2];
             } else {
