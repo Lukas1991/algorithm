4063e8f7962ebe7b68d63e6206ab465e5e123f99
Chuyu <chuyu@Chuyus-MacBook-Pro-4.local>
2017-10-02 17:21:07 -0400

    Temp knightProbability

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,46 +1,94 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.junit.Test;
 
 public class TestExample {
 
+
+    private String encode(int[] arr) {
+        return arr[0] + "," + arr[1];
+    }
+
     @Test
     public void test() {
-        List<String> res = addOperators("123", 6);
-        System.err.println(res.toString());
-//        res = addOperators("105", 5);
-//        System.err.println(res.toString());
-
+        Set<String> curr = new HashSet<>();
+        curr.add(encode(new int[]{1,2}));
+        curr.add(encode(new int[]{1,2}));
+        System.err.println(knightProbability(3,2,0,0));
+        System.err.println(knightProbability(1,1,0,0));
+        System.err.println(knightProbability(10,13,5,3));
     }
 
-    public List<String> addOperators(String num, int target) {
-        List<String> rst = new ArrayList<String>();
-        if(num == null || num.length() == 0) return rst;
-        helper(rst, "", num, target, 0, 0, 0);
-        return rst;
-    }
-    public void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
-        System.err.println(path + " pos: " + pos + " eval: " + eval + " multed: " + multed);
-        if(pos == num.length()){
-            if(target == eval)
-                rst.add(path);
-            return;
-        }
-        for(int i = pos; i < num.length(); i++){
-            if(i != pos && num.charAt(pos) == '0') break;
-            long cur = Long.parseLong(num.substring(pos, i + 1));
 
-            if(pos == 0){
-                helper(rst, path + cur, num, target, i + 1, cur, cur);
+
+    public double knightProbability(int N, int K, int r, int c) {
+        double probability = 1.0;
+        boolean moved = false;
+        List<String> curr = new ArrayList<>();
+        curr.add(r + "," + c);
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        double div = 1.0;
+        for (int i = 0; i < K; i++) {
+            div *= 8.0;
+            List<String> moves = new ArrayList<>();
+
+            for (String s : curr) {
+                if (map.containsKey(s)) {
+                    moves.addAll(map.get(s));
+                } else {
+                    int[] arr = decode(s);
+                    List<String> tmp = getMoves(N, arr[0], arr[1]);
+
+                    moves.addAll(tmp);
+                    map.put(s, tmp);
+                }
             }
-            else{
-                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
 
-                helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
-
-                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+            if (moves.isEmpty()) {
+                break;
+            } else {
+                moved = true;
+                //remove dup
+                Set<String> set = new HashSet<>(moves);
+                curr = new ArrayList<>(set);
+                //curr = moves;
             }
         }
+
+        if (moved) {
+            return curr.size() / div;
+        } else {
+            return 0.0;
+        }
+
     }
 
+    private List<String> getMoves(int N, int r, int c) {
+        List<String> res = new ArrayList<>();
+
+        int[] dx = {-1, -2, -2, -1, 1, 2, 2, 1};
+        int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+        for (int i = 0; i < dx.length; i++) {
+            int x = r + dx[i];
+            int y = c + dy[i];
+
+            if (x >= 0 && y >= 0 && x < N && y < N) {
+                res.add(x + "," + y);
+            }
+        }
+
+        return res;
+    }
+
+    private int[] decode(String s) {
+        String[] arr = s.split(",");
+        return new int[]{Integer.valueOf(arr[0]), Integer.valueOf(arr[1])};
+    }
 }
