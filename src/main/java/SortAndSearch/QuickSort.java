ef9ef2d1b8b8ddc16bde68d54c771105da3a52a7
Chuyu <chuyu@Chuyus-MacBook-Pro-4.local>
2017-10-16 18:20:48 -0400

    quick sort

--- src/main/java/SortAndSearch/QuickSort.java
+++ src/main/java/SortAndSearch/QuickSort.java
@@ -1,77 +1,83 @@
 package SortAndSearch;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
- * 
- * 
+ * QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot.
+ * There are many different versions of quickSort that pick pivot in different ways.
  *
+ * Always pick first element as pivot.
+ * Always pick last element as pivot (implemented below)
+ * Pick a random element as pivot.
+ * Pick median as pivot.
+ *
+ * The key process in quickSort is partition(). Target of partitions is, given an array and an element x of array as pivot, put x at
+ * its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all greater elements
+ * (greater than x) after x. All this should be done in linear time.
+ *
+ * Quick Sort is preferred over MergeSort for sorting Arrays, because 1) in-place sort, doesn't require extra storage. 2) Most practical implementations of Quick Sort use randomized version.
  */
 public class QuickSort {
 
-	public int[] quickSort(int arr[]) {
-		return quickSort(arr,0,arr.length-1);
-	}
-	
-	public int[] quickSort(int arr[],int start,int end){
-		int size=end-start+1;
-		//System.out.format("start: %d end: %d size %d%n",start,end,size);
-		if(size<=1) {return arr;}
-		
-		int L=start-1,U=end+1;
-		//select a pivot randomly
-		//int pivotIndex=(int)(Math.random())*10%size;
-		int pivotIndex=size/2;
-		int pivot=arr[start+pivotIndex];
-		System.out.print("pivot is: "+pivot+" pivotIndex "+(start+pivotIndex)+"\n");
-		while(L<U){
-			L++;
-			while(arr[L]<pivot) L++;
-			U--;
-			while(arr[U]>pivot) U--;
-			
-			//swap 		
-			//System.out.print("swap: "+arr[L]+" "+arr[U]+"\n");
-			if(L<U){
-				int temp=arr[L];arr[L]=arr[U];arr[U]=temp;
-			}
-			
-			for(int a:arr)	System.out.print(a+" ");System.out.print("\n");			
-		}
-		System.out.print("L:"+L+" U:"+U+"\n");
-		
-		quickSort(arr,start,U-1);
-		quickSort(arr,U+1,end);
-		
-		return arr;
-	    
-	}
-	public static void main(String[] args) {
-		QuickSort qs=new QuickSort();
-		
-		/*Random generator = new Random();
-		
-		int[] a = new int[10];
-		for (int i = 0; i < a.length; i++)
-			a[i] = generator.nextInt(100);
-		
-		System.out.println(Arrays.toString(a));
-		*/
-		int[] arr={5,86,69,73,69,19,34,20,74,3};
-		//int[] arr={3,1,4,5,9,2,6,8,7,10};
-		/*int[] arr=new int[10];
-		for(int i=0;i<10;i++){
-			arr[i]=a[i];
-		}*/
-		
-		int[] sorted=qs.quickSort(arr);
-		System.out.println(Arrays.toString(sorted));
-		
-		/*System.out.println(Arrays.toString(a));
-		Arrays.sort(a);
-		System.out.println(Arrays.toString(a));*/
-		
+    public int[] quickSort(int arr[]) {
+        return quickSort(arr, 0, arr.length - 1);
+    }
 
-	}
+    public int[] quickSort(int[] arr, int start, int end) {
+
+        if (start < end) {
+            //pivot is arr[end]
+            //pivotIndex is the partitioning index, arr[pivotIndex] is now at the correct place
+            int pivotIndex = partition(arr, start, end);
+
+            quickSort(arr, start, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, end);
+        }
+
+        return arr;
+
+    }
+
+    private int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        int left = start - 1;
+
+        for (int j = start; j < end; j++) {
+            if (arr[j] <= pivot) {
+                left++;
+                swap(arr, left, j);
+            }
+        }
+
+        swap(arr, left + 1, end);
+        return left + 1;
+    }
+
+    private void swap(int[] a, int i, int j) {
+        final int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        QuickSort qs = new QuickSort();
+
+        int[] arr = {5, 86, 69, 73, 69, 19, 34, 20, 74, 3};
+        int[] sorted = qs.quickSort(arr);
+        System.out.println(Arrays.toString(sorted));
+
+
+        Random generator = new Random();
+
+		int[] a = new int[10];
+		for (int i = 0; i < a.length; i++) {
+            a[i] = generator.nextInt(100);
+        }
+
+		System.out.println("Before sort: " + Arrays.toString(a));
+        a = qs.quickSort(a);
+        System.out.println("After sort: " + Arrays.toString(a));
+    }
 
 }
