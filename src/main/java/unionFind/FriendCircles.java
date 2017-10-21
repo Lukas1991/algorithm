056f826aa61a9420af62313461a17395f0938b17
Chuyu <chuyu@Chuyus-MBP-4.fios-router.home>
2017-10-20 20:54:03 -0400

    findCircleNum2

--- src/main/java/unionFind/FriendCircles.java
+++ src/main/java/unionFind/FriendCircles.java
@@ -39,4 +39,40 @@ public class FriendCircles {
             return a;
         }
     }
+
+    //fill array with its index
+    public int findCircleNum2(int[][] M) {
+        int n = M.length;
+        int count = n;
+        int[] root = new int[n];
+        for (int i = 0; i < n; i++) {
+            root[i] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (M[i][j] == 1) {
+                    int root1 = find2(root, i);
+                    int root2 = find2(root, j);
+
+                    if (root1 != root2) {
+                        //merge
+                        root[root2] = root1;
+                        count--;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private int find2(int[] root, int a) {
+        while (root[a] != a) {
+            int p = root[a];
+            root[a] = root[p];
+            a = root[a];
+        }
+        return a;
+    }
 }
