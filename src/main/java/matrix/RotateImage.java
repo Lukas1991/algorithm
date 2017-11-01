8d2cd1d117af12155febacdedad63f4a16438cec
Chuyu <chuyu@Chuyus-MacBook-Pro-4.local>
2017-11-01 17:16:09 -0400

    RotateImage

--- src/main/java/matrix/RotateImage.java
+++ src/main/java/matrix/RotateImage.java
@@ -1,9 +1,12 @@
 package matrix;
 
+import java.util.Arrays;
+
 public class RotateImage {
 
+    //rotate n*n matrix, Rotate the image by 90 degrees (clockwise).
     //rotate in-place，layer by layer
-    public void rotate(int[][] matrix) {
+    public static void rotate(int[][] matrix) {
         int n = matrix[0].length;
         for (int layer = 0; layer < n / 2; layer++) {
             int i = layer;
@@ -15,36 +18,65 @@ public class RotateImage {
                 //right top
                 int rightTop = matrix[j][n - 1 - i];
                 matrix[j][n - 1 - i] = leftTop;
-                //left bottom
-                int leftBottom = matrix[n - 1 - i][n - 1 - j];
-                matrix[n - 1 - i][n - 1 - j] = rightTop;
                 //right bottom
-                int rightBottom = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = leftBottom;
+                int rightBottom = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = rightTop;
 
-                matrix[i][j] = rightBottom;
+                //left bottom
+                int leftBottom = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = rightBottom;
+
+                matrix[i][j] = leftBottom;
             }
         }
 
+        for (int i = 0; i < matrix.length; i++) {
+            System.err.println(Arrays.toString(matrix[i]));
+        }
     }
 
-    public void rotateDa(int[][] matrix) {
-        int n = matrix.length;
-
+    public static void rotateAntiClockwise(int[][] matrix) {
+        int n = matrix[0].length;
         for (int layer = 0; layer < n / 2; layer++) {
-            int last = n - layer - 1;
+            int i = layer;
 
-            for (int i = layer; i < last; i++) {
-                int offset = i - layer;
-                //leftBottom
-                int tmp = matrix[last - offset][layer]; // left to tmp
+            for (int j = layer; j < n - 1 - layer; j++) {
 
-                matrix[last - offset][layer] = matrix[last][last - offset]; // bottom to left
-                matrix[last][last - offset] = matrix[i][last]; // right to bottom
-                matrix[i][last] = matrix[layer][i]; // top to right
-                matrix[layer][i] = tmp;
+                //left top
+                int leftTop = matrix[i][j];
+
+                //left bottom
+                int leftBottom = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = leftTop;
+
+                //right bottom
+                int rightBottom = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = leftBottom;
+
+                //right top
+                int rightTop = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = rightBottom;
+
+                matrix[i][j] = rightTop;
             }
         }
+
+//        for (int i = 0; i < matrix.length; i++) {
+//            System.err.println(Arrays.toString(matrix[i]));
+//        }
     }
 
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}};
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.err.println(Arrays.toString(matrix[i]));
+        }
+
+        rotate(matrix);
+        //rotateAntiClockwise(matrix);
+    }
 }
