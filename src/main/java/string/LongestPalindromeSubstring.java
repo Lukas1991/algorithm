75d567f2b7891d6d846c157617de43439942c5a1
C <chuyu@Chuyus-MBP.fios-router.home>
2017-11-01 21:30:31 -0400

    LongestPalindromeSubstring

--- src/main/java/string/LongestPalindromeSubstring.java
+++ src/main/java/string/LongestPalindromeSubstring.java
@@ -3,6 +3,7 @@ package string;
 public class LongestPalindromeSubstring {
 
 	/**
+	 * 5. Longest Palindromic Substring
 	 * Iterate the String, get longest palindrome with center of i
 	 * get longest palindrome with center of i, i+1
 	 * time O(n^2), space O(1)	
@@ -39,6 +40,30 @@ public class LongestPalindromeSubstring {
 			end++;
 		}
 		return s.substring(begin + 1, end);
+	}
+
+	/**
+	 * 516. Longest Palindromic Subsequence
+	 * dp[i][j]: the longest palindromic subsequence's length of substring(i, j)
+	 * State transition:
+	 * dp[i][j] = dp[i+1][j-1] + 2 if s.charAt(i) == s.charAt(j)
+	 * otherwise, dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1])
+	 * Initialization: dp[i][i] = 1
+	 */
+	public int longestPalindromeSubseq(String s) {
+		int[][] dp = new int[s.length()][s.length()];
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			dp[i][i] = 1;
+			for (int j = i+1; j < s.length(); j++) {
+				if (s.charAt(i) == s.charAt(j)) {
+					dp[i][j] = dp[i+1][j-1] + 2;
+				} else {
+					dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+				}
+			}
+		}
+		return dp[0][s.length()-1];
 	}
 
 }
