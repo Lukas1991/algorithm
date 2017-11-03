8e51af1775d326f919841de7d02c3b3c2081e4a0
C <chuyu@Chuyus-MBP.fios-router.home>
2017-11-02 21:08:48 -0400

    ShortestPalindrome

--- src/main/java/string/ShortestPalindrome.java
+++ src/main/java/string/ShortestPalindrome.java
@@ -3,6 +3,9 @@ package string;
 public class ShortestPalindrome {
 
     //O(n^2)
+    /**
+     * 为了求最短panlindrome, i 从reverse 的头开始，如果一开始reverse.substring(0) == s.substring(0,n),那么string 本身就是palindrome.
+     */
     public String shortestPalindrome(String s) {
         StringBuilder sb = new StringBuilder(s);
         String reverse = sb.reverse().toString();
