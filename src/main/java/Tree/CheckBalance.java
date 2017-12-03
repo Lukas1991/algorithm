0937ca8f35375c1efa895ac29a30f0c2bbdbb294
Chuyu <chuyu@Chuyus-MBP-4.fios-router.home>
2017-12-02 21:08:28 -0500

    CheckBalance

--- src/main/java/Tree/CheckBalance.java
+++ src/main/java/Tree/CheckBalance.java
@@ -1,92 +1,32 @@
 package Tree;
 
+/**
+ * 110. Balanced Binary Tree
+ */
 public class CheckBalance {
 
 
-	/**
-	 * getHeight could check if the tree is balanced as the same time as it's checking height. if the subtree isn't balanced, return -1
-	 * run time O(N) and O(H) space, where H is the height of the tree
-	 * @param args
-	 */
-	public static boolean isBalancedBest(Node root){
-		if(checkHeight(root) == -1){
-			return false;
-		}else 
-			return true;
-	}
-	
-	
-	public static int checkHeight(Node root){
-		if(root == null){
-			return 0;
-		}
-		//check if left is balanced
-		int leftHeight = checkHeight(root.left);
-		if(leftHeight == -1){
-			return -1; //not balanced
-		}
-		//check if right is balanced
-		int rightHeight = checkHeight(root.right);
-		if(rightHeight == -1){
-			return -1; //not balanced
-		}
-		
-		//check if current node is balanced
-		int diff = leftHeight - rightHeight;
-		if(Math.abs(diff) > 1){
-			return -1; //not balanced
-		}else{
-			return 1 + Math.max(leftHeight, rightHeight); //return height
-		}
-		
-		
-	}
-	
-	
-	/*
-	 * 4.1 check if a tree is balanced
-	 * the difference of min depth and max depth should not exceed 1
-	 * the difference is the maximum distance possible in the tree.
-	 * Cons: On each node, we recurse through its entire subtree. getHeight() is called repeatedly on the same node, O(N^2)
-	 */
-	public static int getHeight(Node root){
-		if(root==null) return 0;
-		return 1+Math.max(getHeight(root.left), getHeight(root.right));
-	}
-	
-	public static boolean isBalanced(Node root){
-		if(root ==null) return true;
-		//int leftHeight=getHeight(root.left);
-		//int rightHeight=getHeight(root.right);
-		//System.out.println("leftHeight: "+leftHeight+" rightHeight: "+rightHeight);
-		int heightDiff=getHeight(root.left)-getHeight(root.right);
-		if(Math.abs(heightDiff)>1) return false;
-		else {
-			System.out.println(isBalanced(root.left)+" "+isBalanced(root.right));
-			//if left is balanced, and right is balanced, return true;
-			//return true;  
-			return isBalanced(root.left) && isBalanced(root.right) ;
-		}
-	}
-	//------------------------------------------------------
-	
-	
-	public static void main(String[] args) {
-		BSTree tree=new BSTree();
-		tree.insert(5);
-		tree.insert(3);
-		tree.insert(9);
-		tree.insert(1);
-		tree.insert(4);
-		tree.insert(6);
-		tree.insert(7);//tree.insert(8);
-				
-		tree.displayTree();
-		
-		
-		boolean result=isBalanced(tree.root);
-		System.out.println("result is+"+result);
+    /**
+     * Bottom up O(N)
+     * checkHeight checks if the tree is balanced and checks tree's height at the same time.
+     * if the subtree isn't balanced, return -1
+     * Time O(N) and O(H) space, where H is the height of the tree
+     */
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
 
-	}
+    private int checkHeight(TreeNode root) {
+        if (root == null) return 0;
 
+        int leftHeight = checkHeight(root.left);
+        if (leftHeight == -1) return -1;//left not balanced
+
+        int rightHeight = checkHeight(root.right);
+        if (rightHeight == -1) return -1;//right not balanced
+
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
