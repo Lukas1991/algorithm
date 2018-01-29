eda954af657e9698af448a96b96f1beced7f3c88
Chuyu <chuyu@Chuyus-MBP-4.fios-router.home>
2018-01-28 19:58:44 -0500

    trapRainWater

--- src/main/java/fun/TrapRainWater.java
+++ src/main/java/fun/TrapRainWater.java
@@ -1,5 +1,7 @@
 package fun;
 
+import java.util.PriorityQueue;
+
 /**
  * Given n non-negative integers representing an elevation map where the width of each bar is 1,
  * compute how much water it is able to trap after raining.
@@ -39,6 +41,80 @@ public class TrapRainWater {
         return count;
     }
 
+    //----------------Trap Rain Water 2-----------------------------------
+    /*
+     * 四条外边放到min heap里，
+     * 每次取min, 扫四向，如果not visited, 判断是否灌水，再加到heap里
+     */
+    public int trapRainWater(int[][] heights) {
+        int rows = heights.length;
+        int cols = heights[0].length;
+
+        PriorityQueue<Cell> minHeap = new PriorityQueue<>((c1, c2) -> c1.h - c2.h);
+        boolean[][] visited = new boolean[rows][cols];
+        putBordersToHeap(heights, minHeap, visited);
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        int res = 0;
+        while (!minHeap.isEmpty()) {
+            Cell cell = minHeap.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int x = cell.i + dx[k];
+                int y = cell.j + dy[k];
+                if (inBound(x, y, rows, cols) && !visited[x][y]) {
+                    if (heights[x][y] < cell.h) { //灌水
+                        res += cell.h - heights[x][y];
+                    }
+
+                    visited[x][y] = true;
+                    minHeap.offer(new Cell(x, y, Math.max(cell.h, heights[x][y]))); //取大的高度，放到heap里
+                }
+            }
+        }
+
+        return res;
+    }
+
+    void putBordersToHeap(int[][] heights, PriorityQueue<Cell> minHeap, boolean[][] visited) {
+        int rows = heights.length;
+        int cols = heights[0].length;
+        //first and last row
+        for (int j = 0; j < cols; j++) {
+            minHeap.offer(new Cell(0, j, heights[0][j]));
+            visited[0][j] = true;
+            minHeap.offer(new Cell(rows - 1, j, heights[rows - 1][j]));
+            visited[rows - 1][j] = true;
+        }
+
+        //first and last column
+        for (int i = 0; i < rows; i++) {
+            minHeap.offer(new Cell(i, 0, heights[i][0]));
+            visited[i][0] = true;
+            minHeap.offer(new Cell(i, cols - 1, heights[i][cols - 1]));
+            visited[i][cols - 1] = true;
+        }
+    }
+
+    boolean inBound(int x, int y, int n, int m) {
+        return x >= 0 && x < n && y >= 0 && y < m;
+    }
+
+    class Cell {
+        int i;
+        int j;
+        int h;
+
+        public Cell(int i, int j, int h) {
+            this.i = i;
+            this.j = j;
+            this.h = h;
+        }
+    }
+
+    //---------------------------------------------------
     //DP, Time O(n), Space O(n)
     public int trap(int[] height) {
         if (height == null || height.length <= 2) {
