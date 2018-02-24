1f7e0f71111869f91bda50ae6fd6ee5e8dd8a6a6
Chuyu <chuyu@Chuyus-MBP-4.fios-router.home>
2018-02-23 20:37:15 -0500

    maxSquare2

--- src/main/java/dp/MaximalSquare.java
+++ src/main/java/dp/MaximalSquare.java
@@ -33,4 +33,52 @@ public class MaximalSquare {
 
         return max * max;
     }
+
+    /**
+     * Given a 2D binary matrix filled with 0's and 1's, find the largest square which diagonal is all 1 and others is 0.
+     */
+    public int maxSquare2(int[][] matrix) {
+        int n = matrix.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int m = matrix[0].length;
+        if (m == 0) {
+            return 0;
+        }
+
+        int[][] dignalOnes = new int[n][m];  //以i, j作为正方形的右下角最长边长
+        int[][] upZeros = new int[n][m]; //从i, j 往上数，有多少个连着的0
+        int[][] leftZeros = new int[n][m];  //从i, j 往左数，有多少个连着的0
+
+        int max = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (matrix[i][j] == 0) {
+                    dignalOnes[i][j] = 0;
+                    upZeros[i][j] = leftZeros[i][j] = 1;
+                    if (i > 0) {
+                        upZeros[i][j] = upZeros[i - 1][j] + 1;
+                    }
+                    if (j > 0) {
+                        leftZeros[i][j] = leftZeros[i][j - 1] + 1;
+                    }
+                } else {
+                    upZeros[i][j] = leftZeros[i][j] = 0;
+
+                    if (i == 0 || j == 0) {
+                        dignalOnes[i][j] = 1;
+                    } else {
+                        //min(斜着1的个数，上面0的个数，左边0的个数)
+                        dignalOnes[i][j] = Math.min(dignalOnes[i - 1][j - 1], Math.min(upZeros[i - 1][j], leftZeros[i][j - 1])) + 1;
+                    }
+                }
+
+                max = Math.max(max, dignalOnes[i][j]);
+            }
+        }
+
+        return max * max;
+    }
 }
