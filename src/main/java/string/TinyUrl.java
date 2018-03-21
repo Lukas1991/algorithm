0208f8b9a241faf496d3f9c6ca7a9d1c4e0f424d
C <chuyu@Chuyus-MBP.fios-router.home>
2018-03-21 11:06:25 -0400

    tiny url

--- src/main/java/string/TinyUrl.java
+++ src/main/java/string/TinyUrl.java
@@ -9,6 +9,7 @@ public class TinyUrl {
     String chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     Map<String, String> map = new HashMap<>();
+    Map<String, String> longToShort = new HashMap<>();
     Random random = new Random();
 
     private String generateRandomKey() {
@@ -21,19 +22,49 @@ public class TinyUrl {
     }
 
     // Encodes a URL to a shortened URL.
-    public String encode(String longUrl) {
+    public String longToShort(String longUrl) {
+        if (longToShort.containsKey(longUrl)) {
+            return longToShort.get(longUrl);
+        }
+
         String key = generateRandomKey();
         while (map.containsKey(key)) {
             key = generateRandomKey();
         }
         map.put(key, longUrl);
-        return key;
+        longToShort.put(longUrl, "http://tiny.url/" + key);
+        return "http://tiny.url/" + key;
     }
 
     // Decodes a shortened URL to its original URL.
-    public String decode(String shortUrl) {
-        String code = shortUrl.replace("http://tinyurl.com/", "");
+    public String shortToLong(String shortUrl) {
+        String code = shortUrl.replace("http://tiny.url/", "");
         return map.get(code);
     }
 
+
+    public String createCustom(String long_url, String key) {
+        if (key.length() < 6) {
+            return "error";
+        }
+        if (map.containsKey(key)) {
+            return "error";
+        }
+
+        map.put(key, long_url);
+        return "http://tiny.url/" + key;
+    }
+
+    public static void main(String[] args) {
+        TinyUrl obj = new TinyUrl();
+        System.err.println(obj.createCustom("http://www.lintcode.com/", "lccode"));
+
+        System.err.println(obj.longToShort("http://www.lintcode.com/problem/"));
+
+        System.err.println(obj.shortToLong("http://tiny.url/lccode"));
+        System.err.println(obj.createCustom("http://www.lintcode.com/", "lc"));
+        System.err.println(obj.createCustom("http://www.lintcode.com/en/ladder/", "lccode"));
+
+        System.err.println(obj.longToShort("http://www.lintcode.com/problem/"));
+    }
 }
