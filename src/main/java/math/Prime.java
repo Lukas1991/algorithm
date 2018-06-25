f4eb534985b4bb45794da3fd8ce7990b5a96fec8
C <chuyu@Chuyus-MacBook-Pro-1431.local>
2018-06-25 14:51:08 -0400

    count prime

--- src/main/java/math/Prime.java
+++ src/main/java/math/Prime.java
@@ -2,48 +2,51 @@ package math;
 
 public class Prime {
 
-	/**
-	 * 2 is prime, 1 is not prime
-	 * only have to go up to the square root of n, 
-	 * because if you list out all of the factors of a number, the square root will always be in the middle 
-	 */
-	public static boolean isPrime(int n){
-		if(n==2) return true;
-		if(n%2 == 0) return false;
-	
-		//check all odd numbers3,5,7,9, to square root
-		for(int i=3; i*i<=n; i+=2){
-			if(n%i == 0) return false;
-		}
-		return true;
-	}
+    /**
+     * 2 is prime, 1 is not prime
+     * only have to go up to the square root of n,
+     * because if you list out all of the factors of a number, the square root will always be in the middle
+     */
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
 
-	public int countPrimes(int n) {
-		// init an array to track prime numbers, set all to true
-		boolean[] isPrime = new boolean[n];
-		for (int i = 2; i<n; i++) {
-			isPrime[i] = true;
-		}
+        //check all odd numbers3,5,7,9, to square root
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-		// Loop's ending condition is i * i < n instead of i < sqrt(n) to avoid repeatedly calling an expensive function sqrt().
-		for (int i=2; i*i < n; i++) {
-			if(isPrime[i]) {
-				for (int j = i*i; j<n; j=j+i) {
-					isPrime[j] = false;
-				}
-			}
-		}
+    public int countPrimes(int n) {
+        // init an array to track prime numbers, set all to true
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
 
-		int count = 0;
-		for (int i=2; i<n; i++) {
-			if (isPrime[i]) count++;
-		}
-		return count;
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Loop's ending condition is i * i < n instead of i < sqrt(n) to avoid repeatedly calling an expensive function sqrt().
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j = j + i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
 
-	}
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
 
 }
