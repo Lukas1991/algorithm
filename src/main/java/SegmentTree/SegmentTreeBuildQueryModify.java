ed2dd6330cf83cfc3d2e6ca36c4d26f3ffb25c93
C <chuyu@Chuyus-MBP.fios-router.home>
2018-07-22 14:17:03 -0400

    segment tree

--- src/main/java/SegmentTree/SegmentTreeBuildQueryModify.java
+++ src/main/java/SegmentTree/SegmentTreeBuildQueryModify.java
@@ -25,6 +25,7 @@ public class SegmentTreeBuildQueryModify {
         }
     }
 
+    //log(n)
     public void modify(SegmentTreeNode root, int index, int value) {
         if (root.start == index && root.end == index) {
             root.max = value;
@@ -41,6 +42,7 @@ public class SegmentTreeBuildQueryModify {
         root.max = Math.max(root.left.max, root.right.max);
     }
 
+    //log(n)
     public int query(SegmentTreeNode root, int start, int end) {
         if (start > end || root == null) {
             return Integer.MIN_VALUE;
@@ -53,11 +55,11 @@ public class SegmentTreeBuildQueryModify {
         int mid = (root.start + root.end) / 2;
         int max = Integer.MIN_VALUE;
 
-        if (mid >= start) {
+        if (mid >= start) {// 如果查询区间和左边节点区间有交集
             max = Math.max(max, query(root.left, start, end));
         }
 
-        if (end > mid) {
+        if (end > mid) {// 如果查询区间和右边节点区间有交集
             max = Math.max(max, query(root.right, start, end));
         }
 
