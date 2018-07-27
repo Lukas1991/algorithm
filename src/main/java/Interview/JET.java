d8a67807c0cc7621544d291a96d3c2f8c853c3aa
C <chuyu@Chuyus-MBP-1672.fios-router.home>
2018-07-26 20:45:56 -0400

    JET

---
+++ src/main/java/Interview/JET.java
@@ -0,0 +1,25 @@
+package Interview;
+
+public class JET {
+    /**
+     * Build a directed graph,
+     * find list of nodes with K parents
+     * find list of nodes with K children
+     *
+     * find lca of two nodes
+     */
+
+    /**
+     * Reverse a sentence, "I ate food" -> "food ate I"
+     *
+     * Implement string.split(" ")
+     */
+
+    /**
+     * Design key value store service
+     */
+
+    /**
+     * Design import a excel of N items, and shows N import result
+     */
+}
